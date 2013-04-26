c48ddc699f776eb2d0a0ae811d452d921428293c
Ean McLaughlin <computertechie2@gmail.com>
2013-04-25 18:32:47 -0600

    Added things to git exclude.

--- out/artifacts/Cubes_jar/basic_block_frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 140
-
-//out vec4 out_Color;// gl_FragColor;
-in vec4 pos;
-
-void main(void) {
-	// Override out_Color with our texture pixel
-    gl_FragColor = pos; //colours based on vertex position - gl_* are builtin variables
-}
