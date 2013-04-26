d4a9de0592c025303abd8699ef0d46cac381c65a
Ean McLaughlin <computertechie2@gmail.com>
2013-04-25 21:37:47 -0600

    Apparently git -a doesn't add untracked files.

---
+++ src/tk/snowmew/cubes/Light.java
@@ -0,0 +1,10 @@
+package tk.snowmew.cubes;
+
+/**
+ * User: Pepper
+ * Date: 4/25/13
+ * Time: 9:33 PM
+ * Project: Cubes
+ */
+public abstract class Light {
+}
---
+++ src/tk/snowmew/cubes/PointLight.java
@@ -0,0 +1,10 @@
+package tk.snowmew.cubes;
+
+/**
+ * User: Pepper
+ * Date: 4/25/13
+ * Time: 9:34 PM
+ * Project: Cubes
+ */
+public class PointLight extends Light {
+}
