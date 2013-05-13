bb300e19add4677a158686e7c2a12e560af2046d
Ean McLaughlin <computertechie2@gmail.com>
2013-05-12 20:46:01 -0600

    Untracked files.

---
+++ src/tk/snowmew/cubes/PointLightPool.java
@@ -0,0 +1,21 @@
+package tk.snowmew.cubes;
+
+/**
+ * User: Pepper
+ * Date: 5/6/13
+ * Time: 9:20 AM
+ * Project: Cubes
+ */
+
+
+public class PointLightPool extends LightPool {
+
+    public PointLightPool(int initialSize){
+        super(initialSize);
+    }
+
+    @Override
+    public int getPoolSizeInFloat() {
+        return lights.size()*PointLight.sizeOf;
+    }
+}
