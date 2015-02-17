eed815382e9d1ccdf8bfb2a7592c4ff62793309c
Ean McLaughlin <computertechie2@gmail.com>
2015-02-17 02:32:44 -0700

    Greatly simplified Camera movement/rotation updating and matrix construction. Better model.

--- src/main/java/link/snowcat/cubes/Cubes.java
+++ src/main/java/link/snowcat/cubes/Cubes.java
@@ -23,27 +23,28 @@ import java.util.Map;
  */
 
 public class Cubes {
-    final int TIME_CONVERSION = 100000000;
+    final int TIME_CONVERSION = 1_000_000, FRAMES = 60;
     public static Renderer renderInstance = Renderer.getInstance();
     public static TextureManager textureManagerInstance = TextureManager.getInstance();
     public static MaterialManager materialManagerInstance = MaterialManager.getInstance();
     public static ShaderProgramManager shaderProgramManager = ShaderProgramManager.getInstance();
+
     private String[] vertAttribs = {"position","in_tex","normal"};
     private String[] uniformAttribs= {"texture","diffuseColor"};
+
     private boolean quit = false;
     Camera camera = new Camera(0,0);
     Model box;
     public DirectionalLight sun = new DirectionalLight(new Vector3f(-10,0,0), new Vector3f(1,1,1), 0.1f);
-    int width =854, height=480;
+    int width =854, height=480, fps = 0;
     float mouseSensitivity = 0.1f;
-    long tick = 0;
+    long physicsTick = 0, renderTick = 0;
 
-    double t = 0, dt = 0.01, currentTime = System.nanoTime()/TIME_CONVERSION, accumulator=0,frameTime =0;
+    double designatedTickTime = 15, currentTime = System.nanoTime()/TIME_CONVERSION, fpsTime = currentTime, availableTime = 0, frameTime = 0;
 
     public static void main(String[] args){
         Cubes cube = new Cubes();
         cube.tick();
-        System.out.println("Quitting");
     }
 
     public Cubes(){
@@ -55,7 +56,7 @@ public class Cubes {
         renderInstance.createProjectionMatrix();
         camera.setPosition(new Vector3f(0, 1, 0));
         shaderProgramManager.registerProgram("standard",new ShaderProgram(this.getClass().getResource("/assets/shaders/standard/standard_vert.glsl"),this.getClass().getResource("/assets/shaders/standard/standard_frag.glsl"),vertAttribs,uniformAttribs));
-        box = new Model(this.getClass().getResource("/assets/models/OBJ/Small_Disc.obj"));
+        box = new Model(this.getClass().getResource("/assets/models/fighter/eurofighter_obj.obj"));
     }
 
     public void createDisplay(){
@@ -66,6 +67,7 @@ public class Cubes {
             GL11.glEnable(GL11.GL_DEPTH_TEST);
             GL11.glDepthFunc(GL11.GL_LEQUAL);
             GL11.glViewport(0, 0, width, height);
+            GL11.glClearColor(0.5f, 0.5f, 0.5f, 0);
             Mouse.create();
             Mouse.setGrabbed(true);
             Keyboard.create();
@@ -80,14 +82,11 @@ public class Cubes {
             width = Display.getWidth();
             height = Display.getHeight();
             renderInstance.createProjectionMatrix();
-            GL11.glViewport(0,0,width, height);
-            camera.reorient();
+            GL11.glViewport(0, 0, width, height);
         }
     }
 
-    public int getWidth(){
-        return width;
-    }
+    public int getWidth(){ return width; }
 
     public int getHeight(){
         return height;
@@ -96,26 +95,29 @@ public class Cubes {
     public void tick(){
         double newTime = 0;
         while(!Display.isCloseRequested()){
-            newTime = System.nanoTime();///TIME_CONVERSION;
+            newTime = System.nanoTime()/TIME_CONVERSION;
             frameTime = newTime - currentTime;
+            if(frameTime > 25)
+                frameTime = 25;
 
-            frameTime /= TIME_CONVERSION;
-            if(frameTime > 0.25)
-                frameTime = 0.25;
-
+            if(currentTime - fpsTime > 1000){
+                Display.setTitle("FPS: "+fps);
+                fps = 0;
+                fpsTime += 1000;
+            }
             currentTime = newTime;
-            accumulator += frameTime;
+            availableTime += frameTime;
 
-            while(accumulator >= dt){
-                getInput();
+            while(availableTime >= designatedTickTime){
                 if(quit) {
                     Display.destroy();
                     return;
                 }
 
-                tick++;
-                camera.reorient();
-                if(tick%10== 0 && tick <=100){
+                physicsTick++;
+
+                getInput();
+                if(physicsTick %10== 0 && physicsTick <=100){
                     float newX=0, newY=0;
                     if(sun.getDirection().x == 10)
                         newX = -9.9f;
@@ -126,38 +128,38 @@ public class Cubes {
                     sun.setDirection(new Vector3f(newX, -newY, 0));
                 }
                 box.update();
-                t+=dt;
-                accumulator -= dt;
+                availableTime -= designatedTickTime;
             }
 
+            fps++;
+            renderTick++;
             checkForResize();
             GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_COLOR_BUFFER_BIT);
-            GL11.glClearColor(0.5f, 0.5f, 0.5f, 0);
             renderInstance.render(box);
             Display.update();
-            Display.sync(60);
         }
     }
 
-
     public void getInput(){
-        float speedModifier = 0.1f;
+        Vector3f movement = new Vector3f();
+
+        float speedModifier = 1f;
         if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
             quit=true;
         if(Keyboard.isKeyDown(Keyboard.KEY_TAB))
-            speedModifier = 0.1f;
+            speedModifier = 0.001f;
         if(Keyboard.isKeyDown(Keyboard.KEY_W))
-            camera.move((float)(0.1f*speedModifier));
+            movement.setZ(movement.getZ() + (0.1f*speedModifier));
         if(Keyboard.isKeyDown(Keyboard.KEY_S))
-            camera.move((float)(-0.1f*speedModifier));
+            movement.setZ(movement.getZ() + (-0.1f * speedModifier));
         if(Keyboard.isKeyDown(Keyboard.KEY_A))
-            camera.strafe((float)(-0.1f*speedModifier));
+            movement.setX(movement.getX() + (0.1f * speedModifier));
         if(Keyboard.isKeyDown(Keyboard.KEY_D))
-            camera.strafe((float)(0.1f*speedModifier));
+            movement.setX(movement.getX() + (-0.1f * speedModifier));
         if(Keyboard.isKeyDown(Keyboard.KEY_SPACE))
-            camera.moveVertical((float)(0.1f*speedModifier));
+            movement.setY(movement.getY() + (-0.1f * speedModifier));
         if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
-            camera.moveVertical((float)(-0.1f*speedModifier));
+            movement.setY(movement.getY() + (0.1f*speedModifier));
         if(Keyboard.isKeyDown(Keyboard.KEY_T))
             Mouse.setGrabbed(!Mouse.isGrabbed());
         if(Keyboard.isKeyDown(Keyboard.KEY_F4)){
@@ -167,18 +169,16 @@ public class Cubes {
                 e.printStackTrace();
             }
         }
+
         if(Keyboard.isKeyDown(Keyboard.KEY_0)){
             camera.zeroBearing();
             camera.zeroPitch();
         }
 
         int dx = Mouse.getDX(), dy = Mouse.getDY();
-        float temp1 = mouseSensitivity * 0.6f + 0.1f;
-        temp1 = (float)Math.pow((double)temp1, 2) * 8F;
-        dy *= temp1;
-        dx *= temp1;
-        camera.pitch(dy);
-        camera.bearing(dx);
+        camera.changePitch(dy);
+        camera.changeBearing(dx);
+        camera.update(movement);
     }
 
     static{
@@ -205,7 +205,6 @@ public class Cubes {
                     Files.copy(file, destination);
 
                     tempFile = new File(destination.toUri());
-                    System.out.println(tempFile.toString());
                     tempFile.deleteOnExit();
                 }
             }
